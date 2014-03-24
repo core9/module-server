290902d4c6337e7908b70e9103494f6c76d794c5
Mark Wienk <markwienk@gmail.com>
2014-03-24 12:10:51 +0100

    Get the virtualhosts indexed from the hostmanager

--- src/api/java/io/core9/plugin/server/HostManager.java
+++ src/api/java/io/core9/plugin/server/HostManager.java
@@ -1,5 +1,7 @@
 package io.core9.plugin.server;
 
+import java.util.Map;
+
 import io.core9.core.plugin.Core9Plugin;
 
 public interface HostManager extends Core9Plugin {
@@ -15,4 +17,10 @@ public interface HostManager extends Core9Plugin {
 	 * @return an array of virtualhosts
 	 */
 	VirtualHost[] getVirtualHosts();
+	
+	/**
+	 * Return the Vhosts with the hostname as index
+	 * @return
+	 */
+	Map<String,VirtualHost> getVirtualHostsByHostname();
 }
